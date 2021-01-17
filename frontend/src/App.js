1e6768d3d3c650575c695c979909df39812d4cf6
bru2313 <bru@adaptiveresponse.net>
2021-01-17 14:22:45 +0000

    2.3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Homescreen from './screens/Homescreen'
  
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
     <Header />
     <main>
         <Container>
-          <h1>Welcome to AdaptiveResponse! </h1>
+          <Homescreen />
       </Container>
      </main>
      <Footer />
