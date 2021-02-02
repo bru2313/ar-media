1d7fcfb44fcba17369ac4be015608a55484d0581
bru2313 <bru@adaptiveresponse.net>
2021-02-02 14:48:14 +0000

    47

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,15 +67,42 @@ const getUserProfile = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmin: user.isAdmin,
-      token: generateToken(user._id)
+      isAdmin: user.isAdmin
     })
   } else {
-    res.status(401)
+    res.status(404)
     throw new Error('User not found')
   }
 
-res.send('Success')
+})
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+ if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if(req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id)
+    })
+
+   } else {
+   res.status(404)
+   throw new Error('User not found')
+ }
 
 })
 
@@ -114,4 +141,4 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 
-export { authUser, registerUser, getUserProfile }
+export { authUser, registerUser, getUserProfile, updateUserProfile }
