d5716b3a8324d20d797dd35b01f6a7e539c62ec2
bru2313 <bru@adaptiveresponse.net>
2021-02-02 18:49:40 +0000

    48

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,6 +39,7 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen.js'
+import ProfileScreen from './screens/ProfileScreen.js'
 
 const App = () => {
   return (
@@ -48,6 +49,7 @@ const App = () => {
         <Container>
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
