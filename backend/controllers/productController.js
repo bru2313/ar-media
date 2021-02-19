d46c47bbe4c784e45cba0cdb6165e0ab829c620a
bru2313 <bru@adaptiveresponse.net>
2021-02-19 16:38:26 +0000

    87

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -301,6 +301,17 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 })
 
+
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+
+  res.json(products)
+})
+
+
 export {
   getProducts,
   getProductById,
@@ -308,4 +319,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts
 }
